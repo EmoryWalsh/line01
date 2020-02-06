6bc132ec435c6617677450fbf800a3216ae12d9d
Emory Walsh <ewalsh00@stuy.edu>
2020-02-06 11:32:47 -0500

    Added most of octant 1 algorithm

--- draw.py
+++ draw.py
@@ -2,3 +2,23 @@ from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
     pass
+
+    var x,y
+    x = x0
+    y = y0
+
+    var A, B
+    #FIX THIS -- WHAT ARE A AND B
+    A = 0 #positive
+    B = 0 #negative
+
+    #octant 1
+    d = 2A + B
+    while x < x1:
+        plot(x, y)
+        if d < 0:
+            y = y + 1
+            d = d + 2B
+        x = x + 1
+        d = d + 2B
+    #end of octant 1
