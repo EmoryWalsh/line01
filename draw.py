75d3dae025c0a78c325ef4cb488de0be99cd311d
Emory Walsh <ewalsh00@stuy.edu>
2020-02-07 11:20:34 -0500

    Octant 5 also working

--- draw.py
+++ draw.py
@@ -2,6 +2,22 @@ from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
 
+    if(x0 > x1):
+        print(str(x0) + "," + str(y0))
+        print(str(x1) + "," + str(y1))
+        storx = x1
+        x1 = x0
+        x0 = storx
+        story = y1
+        y1 = y0
+        y0 = story
+        print(str(x0) + "," + str(y0))
+        print(str(x1) + "," + str(y1))
+
+    #############################################################
+    #HORIZONTAL LINE
+    #if(y0 === y)
+
     #variables x,y
     x = x0
     y = y0
@@ -17,7 +33,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     #if statement in octant 1
     d = 2*A + B
     while x < x1:
-        plot(screen, color, x, y)
+        plot(screen, color, int(x), int(y))
         if d < 0:
             y = y + 1
             d = d + 2*B
