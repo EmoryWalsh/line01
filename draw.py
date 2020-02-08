4eb8c4d35f06a7e61afcf5fa17faa36c65d070da
Emory Walsh <ewalsh00@stuy.edu>
2020-02-08 11:01:08 -0500

    If statement for octants 1,5 added

--- draw.py
+++ draw.py
@@ -11,6 +11,10 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         y1 = y0
         y0 = storey
 
+    #variables A, B
+    A = y1 - y0 #positive
+    B = -(x1 - x0) #negative
+
     #variables x,y
     x = x0
     y = y0
@@ -23,21 +27,31 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             x = x + 1
 
     #VERTICAL LINE
-    if(x0 == x1):
+    elif(x0 == x1):
         while(y < y1):
             plot(screen, color, int(x), int(y))
             y = y + 1
 
 
-    #variables A, B
-    #FIX THIS -- WHAT ARE A AND B
-    A = y1 - y0 #positive
-    B = -(x1 - x0) #negative
 
     #############################################################
-    #OCTANT 1
+    #OCTANT 1,5
 
-    #if statement in octant 1
+    #if statement in octant 1,5
+    elif(abs((y1-y0)/(x1-x0)) < 1):
+        d = 2*A + B
+        while x < x1:
+            plot(screen, color, int(x), int(y))
+            if d < 0:
+                y = y + 1
+                d = d + 2*B
+            x = x + 1
+            d = d + 2*B
+
+    ###############################################################
+    #OCTANT 2,6
+
+    #if statement in octant 2,6
     d = 2*A + B
     while x < x1:
         plot(screen, color, int(x), int(y))
