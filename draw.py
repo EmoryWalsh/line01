eae9e7056cc10ef56fcf4fe9496e76a1af236c7a
Emory Walsh <ewalsh00@stuy.edu>
2020-02-08 22:29:45 -0500

    Think that most is working. Need to check.

--- draw.py
+++ draw.py
@@ -2,63 +2,100 @@ from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
 
-    #SETUP
-    if(x0 > x1):
-        storex = x1
-        x1 = x0
-        x0 = storex
-        storey = y1
-        y1 = y0
-        y0 = storey
-
-    #variables A, B
-    A = y1 - y0 #positive
-    B = -(x1 - x0) #negative
-
     #variables x,y
     x = x0
     y = y0
 
-    #############################################################
+    #so x0 is always smaller than x1
+    if(x0 > x1):
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+
     #HORIZONTAL LINE
     if(y0 == y1):
-        while(x < x1):
+        while(x <= x1):
             plot(screen, color, int(x), int(y))
             x = x + 1
 
     #VERTICAL LINE
     elif(x0 == x1):
-        while(y < y1):
+        while(y <= y1):
             plot(screen, color, int(x), int(y))
             y = y + 1
 
 
+    else:
 
-    #############################################################
-    #OCTANT 1,5
+        #SETUP
+        #variables A, B
+        A = y1 - y0 #positive
+        B = -(x1 - x0) #negative
 
-    #if statement in octant 1,5
-    elif(abs((y1-y0)/(x1-x0)) < 1):
-        d = 2*A + B
-        while x < x1:
-            plot(screen, color, int(x), int(y))
-            if d < 0:
+        #slope
+        slope = (y1-y0)/(x1-x0)
+
+        #OCTANT 1,5
+        #if statement in octant 1,5
+        if(slope < 1 and slope > 0):
+            d = 2*A + B
+            while x <= x1:
+                plot(screen, color, int(x), int(y))
+                if d > 0:
+                    y = y + 1
+                    d = d + 2*B
+                x = x + 1
+                d = d + 2*B
+
+
+        #OCTANT 2,6
+        #if statement in octant 2,6
+        if(slope > 1):
+            d = A + 2*B
+            while y <= y1:
+                plot(screen, color, int(x), int(y))
+                if d < 0:
+                    x = x + 1
+                    d = d + 2*A
                 y = y + 1
                 d = d + 2*B
-            x = x + 1
-            d = d + 2*B
 
-    ###############################################################
-    #OCTANT 2,6
 
-    #if statement in octant 2,6
-    d = 2*A + B
-    while x < x1:
-        plot(screen, color, int(x), int(y))
-        if d < 0:
-            y = y + 1
-            d = d + 2*B
-        x = x + 1
-        d = d + 2*B
+        #OCTANT 3, 7
+        #if statement in octant 3,7
+        if(slope < -1):
+            d = A + 2*B
+            while y >= y1:
+                plot(screen, color, int(x), int(y))
+                if d > 0:
+                    x = x + 1
+                    d = d - 2*A
+                y = y - 1
+                d = d - 2*B
 
-    ###############################################################
+        #OCTANT 4, 8
+        #if statement in octant 4,8
+        if(slope < 0 and slope > -1):
+            d = 2*A + B
+            while x <= x1:
+                plot(screen, color, int(x), int(y))
+                if d < 0:
+                    y = y - 1
+                    d = d - 2*B
+                x = x + 1
+                d = d + 2*A
+
+
+        #NOT IN AN OCTANT
+        #if slope is 1
+        if(slope == 1):
+            while x <= x1:
+                plot(screen, color, int(x), int(y))
+                x = x + 1
+                y = y + 1
+
+        #if slope is -1
+        if(slope == -1):
+            while x <= x1:
+                plot(screen, color, int(x), int(y))
+                x = x + 1
+                y = y - 1
