c5e5255200d87afefcd0c68a3627c8fa495fdb63
Emory Walsh <ewalsh00@stuy.edu>
2020-02-08 22:43:45 -0500

    A few lines aren't drawing and I'm not sure why

--- draw.py
+++ draw.py
@@ -16,86 +16,95 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         while(x <= x1):
             plot(screen, color, int(x), int(y))
             x = x + 1
+        return
 
     #VERTICAL LINE
-    elif(x0 == x1):
+    if(x0 == x1):
         while(y <= y1):
             plot(screen, color, int(x), int(y))
             y = y + 1
+        return
 
 
-    else:
+    #SETUP
+    #variables A, B
+    A = y1 - y0 #positive
+    B = -(x1 - x0) #negative
 
-        #SETUP
-        #variables A, B
-        A = y1 - y0 #positive
-        B = -(x1 - x0) #negative
-
-        #slope
-        slope = (y1-y0)/(x1-x0)
-
-        #OCTANT 1,5
-        #if statement in octant 1,5
-        if(slope < 1 and slope > 0):
-            d = 2*A + B
-            while x <= x1:
-                plot(screen, color, int(x), int(y))
-                if d > 0:
-                    y = y + 1
-                    d = d + 2*B
-                x = x + 1
-                d = d + 2*B
+    #slope
+    slope = (y1-y0)/(x1-x0)
 
 
-        #OCTANT 2,6
-        #if statement in octant 2,6
-        if(slope > 1):
-            d = A + 2*B
-            while y <= y1:
-                plot(screen, color, int(x), int(y))
-                if d < 0:
-                    x = x + 1
-                    d = d + 2*A
+
+    #OCTANT 1,5
+    #if statement in octant 1,5
+    if(slope > 0 and slope < 1):
+        d = 2*A + B
+        while x <= x1:
+            plot(screen, color, int(x), int(y))
+            if d > 0:
                 y = y + 1
                 d = d + 2*B
+            x = x + 1
+            d = d + 2*A
+        return
 
 
-        #OCTANT 3, 7
-        #if statement in octant 3,7
-        if(slope < -1):
-            d = A + 2*B
-            while y >= y1:
-                plot(screen, color, int(x), int(y))
-                if d > 0:
-                    x = x + 1
-                    d = d - 2*A
-                y = y - 1
-                d = d - 2*B
-
-        #OCTANT 4, 8
-        #if statement in octant 4,8
-        if(slope < 0 and slope > -1):
-            d = 2*A + B
-            while x <= x1:
-                plot(screen, color, int(x), int(y))
-                if d < 0:
-                    y = y - 1
-                    d = d - 2*B
+    #OCTANT 2,6
+    #if statement in octant 2,6
+    if(slope > 1):
+        d = A + 2*B
+        while y <= y1:
+            plot(screen, color, int(x), int(y))
+            if d < 0:
                 x = x + 1
                 d = d + 2*A
+            y = y + 1
+            d = d + 2*B
+        return
 
 
-        #NOT IN AN OCTANT
-        #if slope is 1
-        if(slope == 1):
-            while x <= x1:
-                plot(screen, color, int(x), int(y))
+    #OCTANT 3, 7
+    #if statement in octant 3,7
+    if(slope < -1):
+        d = A - 2*B
+        while y >= y1:
+            plot(screen, color, int(x), int(y))
+            if d > 0:
                 x = x + 1
-                y = y + 1
+                d = d + 2*A
+            y = y - 1
+            d = d - 2*B
+        return
 
-        #if slope is -1
-        if(slope == -1):
-            while x <= x1:
-                plot(screen, color, int(x), int(y))
-                x = x + 1
+
+    #OCTANT 4, 8
+    #if statement in octant 4,8
+    if(slope < 0 and slope > -1):
+        d = 2*A - B
+        while x <= x1:
+            plot(screen, color, int(x), int(y))
+            if d < 0:
                 y = y - 1
+                d = d - 2*B
+            x = x + 1
+            d = d + 2*A
+        return
+
+
+    #NOT IN AN OCTANT
+    #if slope is 1
+    if(slope == 1):
+        while x <= x1:
+            plot(screen, color, int(x), int(y))
+            x = x + 1
+            y = y + 1
+        return
+
+    #if slope is -1
+    if(slope == -1):
+        while x <= x1:
+            plot(screen, color, int(x), int(y))
+            x = x + 1
+            y = y - 1
+        return
